d7274f312995416030fc96eb4ba977a603ff6bf8
jeffRnR <jeffmunyigi@gmail.com>
2025-01-19 12:04:55 +0300

    reverte the t in index.html file

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php include 'includes/header.php'; ?>
-<h1>Welcome t to the College Management System</h1>
+<h1>Welcome to the College Management System</h1>
 <a href="templates/register.php">Register</a> | <a href="templates/login.php">Login</a>
 <?php include 'includes/footer.php'; ?>
